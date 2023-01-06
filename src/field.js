dfb953e6210e12f9ca1f961d13d2d52ce2b6bd80
saradeveloper <113747290+saradeveloper@users.noreply.github.com>
2023-01-06 15:01:51 +0900

    code refactoring2

--- src/field.js
+++ src/field.js
@@ -1,8 +1,13 @@
 'use strict';
 import * as sound from './sound.js';
+
 const CARROT_SIZE = 80;
 
-export default class Field {
+export const ItemType = Object.freeze({
+  carrot: 'carrot',
+  bug: 'bug',
+});
+export class Field {
   constructor(carrotCount, bugCount) {
     this.carrotCount = carrotCount;
     this.bugCount = bugCount;
@@ -44,7 +49,7 @@ export default class Field {
     if (target.matches('.carrot')) {
       target.remove();
       sound.playCarrot();
-      this.onItemClick && this.onItemClick('carrot');
+      this.onItemClick && this.onItemClick(ItemType.carrot);
     } else if (target.matches('.bug')) {
       this.onItemClick && this.onItemClick('bug');
     }
